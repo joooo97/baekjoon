2ce810464349d14574422a77081b46de301097b0
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-01-13 17:52:37 +0900

    update: [16928] 뱀과 사다리 게임 solution
    백준 16928번 복습 및 풀이 소스코드 수정

--- graph_traversal/[16928] 뱀과 사다리 게임/Main.java
+++ graph_traversal/[16928] 뱀과 사다리 게임/Main.java
@@ -1,56 +1,53 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static int n, m;
-	public static int[] next = new int[101];
-	public static int[] dist = new int[101]; // 최단 거리, 즉 주사위 굴린 최소 횟수
 
+	public static int n, m;
+	public static int[] board = new int[101]; // 사다리, 뱀 정보
+	public static int[] d = new int[101]; // 주사위를 굴린 횟수
+	
 	public static void bfs() {
 		Queue<Integer> q = new LinkedList<>();
 		q.offer(1);
-		dist[1] = 0;
-
+		d[1] = 0;
+		
 		while(!q.isEmpty()) {
 			int x = q.poll();
-
-			// 주사위 굴리기
-			for(int i = 1; i <= 6; i++) {
+			
+			for(int i = 1; i <= 6; i++) { // 주사위 굴리기
 				int y = x + i;
 				
 				if(y > 100) continue;
-
-				// 다음 칸에 뱀 또는 사다리가 있다면
-				if(next[y] != 0) y = next[y];
+				
+				// 다음 이동 칸에 뱀 또는 사다리가 존재하는 경우
+				if(board[y] != 0) y = board[y];
 				
 				// 다음 이동 칸이 방문하지 않은 칸이라면
-				if(dist[y] == -1) {
+				if(d[y] == -1) {
 					q.offer(y);
-					dist[y] = dist[x] + 1;
+					d[y] = d[x] + 1;
 				}
 			}
 		}
-
 	}
-
+	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());	
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());		
-
+		m = Integer.parseInt(st.nextToken());
+		Arrays.fill(d, -1);
+		
+		// 사다리, 뱀 정보 입력
 		for(int i = 0; i < n+m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-
-			next[x] = y;
+			board[x] = y;
 		}
-
-		Arrays.fill(dist, -1);
 		
 		bfs();
-
-		System.out.println(dist[100]);
+		System.out.println(d[100]);
 	}
 }
