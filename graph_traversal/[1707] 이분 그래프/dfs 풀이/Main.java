a112664522a832cf3d4cebaa7ec8874275b2bbad
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-01-28 18:41:27 +0900

    update: [1707] 이분 그래프 solution
    백준 1706번 복습 및 풀이 소스코드 수정

--- graph_traversal/[1707] 이분 그래프/dfs 풀이/Main.java
+++ graph_traversal/[1707] 이분 그래프/dfs 풀이/Main.java
@@ -3,46 +3,44 @@ import java.util.*;
 
 public class Main {
 	public static ArrayList<ArrayList<Integer>> graph;
-	public static int[] group; // 0: 그룹 없음(방문 x) / 1: 그룹 1 / 2: 그룹 2
-	public static boolean check; // 이분 그래프인지 체크할 변수
-	
-	public static void dfs(int x, int g) {
-		group[x] = g; // 방문 처리 및 그룹 지정
-		
-		// 인접 노드 확인
+	public static int[] group; // 0: 그룹 없음, 1: 그룹 1, -1: 그룹 2
+	public static StringBuilder sb = new StringBuilder();
+
+	public static boolean dfs(int x, int g) {
+		group[x] = g; // 정점 x를 g 그룹으로 지정(= 방문 처리)
+
 		for(int i = 0; i < graph.get(x).size(); i++) {
-			int y = graph.get(x).get(i);
-			
-			// 인접 노드와 그룹이 같다면 이분 그래프가 아님
-			if(group[y] == g) {
-				check = false;
-				return;
+			int y = graph.get(x).get(i); // 인접 정점
+
+			// 인접 정점과 그룹이 같다면 이분 그래프가 아닌 것
+			if(group[y] == g) return false;
+
+			// 인접 정점의 그룹이 아직 정해지지 않은 경우 x와 다른 그룹으로 지정
+			if(group[y] == 0) {
+				if(!dfs(y, -g)) return false;
 			}
-			
-			// 인접 노드에 방문하지 않았다면 방문, 현재 노드와 다른 그룹으로 지정
-			if(group[y] == 0) dfs(y, 3 - g);
 		}
+
+		return true;
 	}
-	
+
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
 		int t = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
 		while(t-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			int v = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken()); // 정점 개수
+			int e = Integer.parseInt(st.nextToken()); // 간선 개수
 			group = new int[v + 1];
-			check = true;
-			
+
 			// 그래프 초기화
-			graph = new ArrayList<ArrayList<Integer>>();
+			graph = new ArrayList<>();
 			for(int i = 0; i <= v; i++) {
 				graph.add(new ArrayList<Integer>());
 			}
-			
+
 			// 간선 연결
 			for(int i = 0; i < e; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -51,16 +49,22 @@ public class Main {
 				graph.get(a).add(b);
 				graph.get(b).add(a);
 			}
-			
-			// 이분 그래프인지 확인
+
+			// dfs 또는 bfs 탐색을 통해 이분 그래프인지 확인
+			// 단절 그래프일 수도 있으므로 아직 방문하지 않은 모든 정점에 대해 탐색 진행
+			boolean ok = true; // 이분 그래프인지 확인
 			for(int i = 1; i <= v; i++) {
-				if(group[i] == 0 && check) dfs(i, 1);
+				// 아직 방문하지 않았다면(= 그룹 미지정) 탐색 진행
+				if(group[i] == 0) {
+					if(!dfs(i, 1)) {
+						ok = false;
+						break;
+					}
+				}
 			}
-			
-			if(!check) sb.append("NO").append("\n");
-			else sb.append("YES").append("\n");
+			if(ok) sb.append("YES").append("\n");
+			else sb.append("NO").append("\n");
 		}
-		
 		System.out.println(sb);
 	}
 }
