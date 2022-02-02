1935245e56b28236f337e155ad5edcc2f51e26af
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-02-03 03:35:53 +0900

    update: [16947] 서울 지하철 2호선 solution
    백준 16947번 복습 및 풀이 소스코드 수정

--- graph_traversal/[16947] 서울 지하철 2호선/Main.java
+++ graph_traversal/[16947] 서울 지하철 2호선/Main.java
@@ -1,64 +1,72 @@
 import java.io.*;
 import java.util.*;
 
+// 1. DFS를 진행하여 순환선에 속하는 역(사이클에 포함되는 정점)찾기
+//  - 사이클에 포함되는 정점은 거리를 0으로 저장 후 큐에 넣기
+// 2. BFS를 진행하여 지선에 속하는 역들의 거리 저장
+//  - 지선에 속하는 역들의 거리는 순환선과의 최단 거리가 됨
 public class Main {
 	public static int n;
 	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 	public static boolean[] visited;
-	public static Queue<Integer> q = new LinkedList<>(); // 순환선에 포함되는 정점을 담을 큐
 	public static int[] dist; // 순환선과의 거리
-	
-	public static int dfs(int x, int prev) {
+	public static Queue<Integer> q = new LinkedList<>();
+
+	// dfs: 순환선에 속하는 역 찾기(사이클에 포함되는 정점 찾기)
+	// -2 반환: 이미 사이클에 포함되는 정점들을 모두 찾은 경우
+	// -1 반환: 사이클을 찾지 못한 경우
+	// 1 ~ n 반환: 사이클의 시작 인덱스
+	public static int dfs(int prev, int x) {
 		visited[x] = true;
 		
 		for(int i = 0; i < graph.get(x).size(); i++) {
 			int y = graph.get(x).get(i);
 			
-			// 1. 다음 정점을 이미 방문한 경우
+			// y가 이미 방문한 정점인 경우 사이클인지 확인
 			if(visited[y]) {
-				// 1-1. 다음 정점과 이전 방문 정점이 다른 경우 - 사이클을 찾은 것
-				if(y != prev) {
-					// 현재 정점은 사이클에 포함됨
-					q.offer(x);
+				if(y != prev) { // y -> z -> x -> y 순으로 방문 (= 사이클을 찾은 것)
+					// - 현재 정점 x는 사이클에 포함되는 정점(x의 거리는 0, 큐에 넣기)
+					// - 사이클을 찾았으므로 사이클의 시작 정점 y를 리턴
 					dist[x] = 0;
-					return y; // 다음 정점을 사이클의 시작 정점으로 정하고 반환
+					q.offer(x);
+					return y;
 				}
-				// 1-2. 다음 정점과 이전 방문 정점이 같은 경우 - 사이클이 아님
-				//  -> 다음 for문 진행하여 사이클을 찾아야 함
-			} else { // 2. 다음 정점을 아직 방문하지 않은 경우
-				int res = dfs(y, x); // 다음 정점 방문
-				// res: -1 또는 0 또는 사이클에 포함되는 정점의 인덱스
+				// y == prev라면 y -> x -> y 순으로 방문한 것 (= 사이클이 아님)
+				// - for문을 돌며 x와 연결된 또 다른 정점 탐색
+			} else { // y에 아직 방문하지 않은 경우
+				// 방문 후 반환된 결과에 따라 처리
+				int res = dfs(x, y);
+
+				// 1. 이미 사이클에 포함되는 정점들을 모두 찾은 경우
+				// - 다른 정점으로의 탐색을 더 이상 진행하지 않고 -2를 계속 반환 
+				if(res == -2) return -2;
+				// 2. 아직 사이클을 찾지 못했으므로 다른 정점으로의 탐색을 계속 진행
+				if(res == -1) continue;
 				
-				// 2-1. 사이클에 포함되는 정점들을 이미 모두 구한 경우
-				if(res == -1) return -1; // 더 이상 구하지 않아도 됨
-				
-				// 2-2. 다음 정점 y는 사이클에 포함되지 않는 정점이므로 다른 정점을 확인해야 함
-				if(res == 0) continue;
-				
-				// 2-3. 현재 정점이 사이클에 포함되는 경우
-				q.offer(x);
+				// 3. res > 0인 경우(= 사이클의 시작 정점이 반환된 경우)
+				// - 현재 정점 x는 사이클에 포함되는 정점 (x의 거리는 0, 큐에 넣기)
 				dist[x] = 0;
+				q.offer(x);
 				
-				// 2-3-1. 현재 정점이 사이클에 포함되는 마지막 정점인 경우
-				if(res == x) return -1; // 더 이상 구하지 않아도 됨
-				
-				// 2-3-2. 사이클에 포함되는 정점이 더 존재하는 경우
-				return res; // 사이클에 포함되는 또 다른 정점 구하기
+				if(x == res) return -2; // 3-1. x는 사이클의 시작 정점이므로 사이클에 포함되는 정점을 모두 찾았으므로 -2 리턴
+				return res; // 3-2. 사이클에 포함되는 정점이 더 있으므로 시작 정점의 인덱스를 계속 반환
 			}
-			
-		}
-		return 0; // 사이클에 포함되지 않는 정점
+		} // for
+		return -1; // 사이클을 찾지 못한 경우
 	}
 	
+	// 지선에 속하는 정점의 거리 = 순환선과의 최단 거리
 	public static void bfs() {
 		while(!q.isEmpty()) {
 			int x = q.poll();
 			
 			for(int i = 0; i < graph.get(x).size(); i++) {
 				int y = graph.get(x).get(i);
+				// 거리가 아직 기록되지 않은 정점(지선)이면 거리 기록 후 큐에 넣기
+				// = 아직 방문하지 않았다면 큐에 넣기
 				if(dist[y] == -1) {
-					q.offer(y);
 					dist[y] = dist[x] + 1;
+					q.offer(y);
 				}
 			}
 		}
@@ -67,13 +75,13 @@ public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		visited = new boolean[n+1];
-		dist = new int[n+1];
-		Arrays.fill(dist, -1);
+		visited = new boolean[n + 1];
+		dist = new int[n + 1];
 		
-		// 그래프 초기화
+		// 그래프, 거리배열 초기화
 		for(int i = 0; i <= n; i++) {
 			graph.add(new ArrayList<Integer>());
+			dist[i] = -1;
 		}
 		
 		// 간선 연결
@@ -82,22 +90,17 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
 			graph.get(a).add(b);
 			graph.get(b).add(a);
 		}
 		
-		// 사이클(순환선) 찾기
-		dfs(1, 0);
-		
-		// 각 역과 순환선 사이의 거리 구하기
-		bfs();
+		dfs(0, 1); // 사이클(순환선) 찾기
+		bfs(); // 지선들의 거리(순환선과의 거리) 구하기
 		
 		StringBuilder sb = new StringBuilder();
 		for(int i = 1; i <= n; i++) {
 			sb.append(dist[i]).append(" ");
 		}
-
 		System.out.println(sb);
 	}
 }
