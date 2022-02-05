cb0cbfedecb0f89f8efd5b97276a7635812c1118
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-02-05 19:20:36 +0900

    update: [16929] Two Dots solution
    백준 16929번 복습 및 풀이 소스코드 수정

--- graph_traversal/[16929] Two Dots/Main.java
+++ graph_traversal/[16929] Two Dots/Main.java
@@ -1,52 +1,62 @@
 import java.io.*;
 import java.util.*;
 
+// 문제에서 주어진 사이클의 정의
+// 1. 모든 점의 색은 같다
+// - 같은 색의 그룹별로 탐색 진행하여 사이클의 유무 확인해야 함
+//  -> for문을 돌며 방문하지 않은 모든 정점에 대해 dfs탐색
+// 2. 사이클에 포함되는 정점은 4개 이상이다.
+// - 상하좌우로만 이동 가능하기 때문에 사이클을 찾은 경우 정점은 최소 4개 이상이 됨
+//  -> 굳이 사이클에 포함되는 각 정점들과 총 개수를 알 필요가 없을 듯
+//  -> 지하철 2호선 문제같이 int로 사이클 시작 정점을 반환하며 현재 정점과 비교하지 않아도 될 듯
 public class Main {
 	public static int n, m;
-	public static char[][] board;
+	public static char[][] color;
 	public static boolean[][] visited;
 	public static int[] dx = {-1, 1, 0, 0};
 	public static int[] dy = {0, 0, -1, 1};
-	
-	public static void dfs(int x, int y, int prev_x, int prev_y) {
+
+	public static void dfs(int px, int py, int x, int y) {
 		visited[x][y] = true;
-		
+
 		for(int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
-			
-			// 게임판의 범위를 넘어가면 무시
+
 			if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			// 색이 다르다면 무시
-			if(board[x][y] != board[nx][ny]) continue;
-			// 다음 이동 칸이 이전 이동 칸이라면 사이클이 될 수 없으므로 무시
-			if(nx == prev_x && ny == prev_y) continue;
-			
-			// 사이클을 찾은 경우 (다음 이동 칸이 이미 방문한 칸이라면)
+
+			// 색이 다르면 무시
+			if(color[nx][ny] != color[x][y]) continue;
+
+			// 다음 이동 정점을 이미 방문했다면 사이클인지 확인
 			if(visited[nx][ny]) {
-				System.out.println("Yes");
-				System.exit(0);
+				// 다음으로 이동할 정점이 이전 방문 정점과 다르다면 사이클을 찾은 것
+				if(nx != px || ny != py) {
+					System.out.println("Yes");
+					System.exit(0);
+				}
+			} else { // 다음 이동 정점에 방문하지 않은 경우 방문
+				dfs(x, y, nx, ny);
 			}
-			dfs(nx, ny, x, y);
-		}		
+		}
 	}
-	
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		board = new char[n][m];
+		color = new char[n][m];
 		visited = new boolean[n][m];
-		
+
 		for(int i = 0; i < n; i++) {
-			board[i] = br.readLine().toCharArray();
+			color[i] = br.readLine().toCharArray();
 		}
-		
+
+		// 같은 색의 그룹별로 탐색 진행하여 사이클의 유무 확인
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
-				// 연결된 그래프(같은 색이 인접한 그래프)마다 탐색하기 위해 방문 여부 확인
-				if(!visited[i][j]) dfs(i, j, -1, -1);
+				if(!visited[i][j]) dfs(-1, -1, i, j);
 			}
 		}
 		System.out.println("No");
