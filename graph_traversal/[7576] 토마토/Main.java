1167717b23cfdeb8bff898ce8faf874bb2f81dd4
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-02-16 17:55:35 +0900

    update: [7576] 토마토 solution
    백준 7576번 복습 및 풀이 소스코드 수정

--- graph_traversal/[7576] 토마토/Main.java
+++ graph_traversal/[7576] 토마토/Main.java
@@ -1,11 +1,15 @@
 import java.io.*;
 import java.util.*;
 
+// 왜 BFS를 이용하여 최단 경로를 구하는 문제인가?
+// - 인접한 토마토가 익게 되고, 토마토가 모두 익는 최소 일 수를 구해야 함
+// - 익은 토마토들의 위치를 모두 큐에 먼저 담은 후 BFS를 진행
+
 class Tomato {
 	int x;
 	int y;
 	int day;
-
+	
 	Tomato(int x, int y, int day) {
 		this.x = x;
 		this.y = y;
@@ -15,65 +19,64 @@ class Tomato {
 
 public class Main {
 	public static int n, m, ans;
-	public static int[][] box;
+	public static int[][] box; // 1:익음, 0:익지 않음, -1:토마토 x
 	public static int[] dx = {-1, 1, 0, 0};
 	public static int[] dy = {0, 0, -1, 1};
 	public static Queue<Tomato> q = new LinkedList<>();
-
-	public static void bfs() {
-		int day = 0; // 모두 익을 때까지의 최소 날짜
-
+	
+	
+	public static void bfs() {		
 		while(!q.isEmpty()) {
 			Tomato t = q.poll();
 			int x = t.x;
 			int y = t.y;
-			int d = t.day;
-			day = d; // 탐색 진행하며 날짜 갱신
+			int day = t.day;
+			// 익은 토마토가 큐에서 꺼내지므로 최소 일 수 갱신
+			ans = Math.max(ans, day); // 최소 일 수 갱신
 			
 			for(int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
 				
 				if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-
-				if(box[nx][ny] == 0) {
-					box[nx][ny] = 1; // 토마토 익히기
-					q.offer(new Tomato(nx, ny, d + 1));					
+				
+				if(box[nx][ny] == 0) { // 인접한 토마토가 익지 않은 토마토인 경우
+					box[nx][ny] = 1; // 익히기
+					q.offer(new Tomato(nx, ny, day + 1));
 				}
 			}
-		}
-		ans = day;
+		}		
 	}
-
-	public static void main(String[] args) throws IOException {
+	
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		m = Integer.parseInt(st.nextToken());
 		n = Integer.parseInt(st.nextToken());
 		box = new int[n][m];
-
-		boolean ok = true; // 처음부터 모두 익었는지 확인할 변수
+		
+		boolean check = true; // 처음부터 모두 익었는지 확인할 변수
 		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < m; j++) {
 				box[i][j] = Integer.parseInt(st.nextToken());
-
+				
 				// 익지 않은 토마토가 주어졌는지 체크
-				if(box[i][j] == 0) ok = false;
-				// 익은 토마토의 위치 기록
+				if(box[i][j] == 0) check = false;
+				// 익은 토마토라면 큐에 넣기
 				if(box[i][j] == 1) q.offer(new Tomato(i, j, 0));
 			}
 		}
-
-		// 처음부터 모두 익어있다면 0 출력 후 종료
-		if(ok) {
+		
+		// 모든 토마토가 처음부터 익어있는 상태
+		if(check) {
 			System.out.println(0);
 			System.exit(0);
 		}
-
-		bfs(); // 토마토 익히기
 		
-		// 모두 익지는 못하는 상황인지 확인
+		bfs(); // 토마토 익히기
+
+		// 토마토가 모두 익지는 못하는 상황인지 확인
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
 				if(box[i][j] == 0) {
