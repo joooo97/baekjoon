4cc8e6773320ec61ba27cebebb705a359a4c8cbf
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-02-17 12:42:03 +0900

    update: [13549] 숨바꼭질 solution
    백준 13549번 복습 및 풀이 소스코드 수정

--- graph_traversal/[13549] 숨바꼭질 3/Main.java
+++ graph_traversal/[13549] 숨바꼭질 3/Main.java
@@ -1,6 +1,12 @@
 import java.io.*;
 import java.util.*;
 
+// 구하는 문제: 수빈이가 동생을 찾을 수 있는 가장 빠른 시간
+// - 정점: 수빈이와 동생의 위치
+// - 가중치: 시간이며, 주어진 가중치는 0 또는 1로 다르다.
+//  -> 가중치가 다른 문제에서의 최단 경로를 구하는 문제이다.
+//  -> 가중치가 작은 것부터 먼저 처리해야 한다.(큐에 넣어줘야 함)
+
 class Node {
 	int x;
 	int t;
@@ -8,7 +14,7 @@ class Node {
 	Node(int x, int t) {
 		this.x = x;
 		this.t = t;
-	}
+	}	
 }
 
 public class Main {
@@ -16,46 +22,48 @@ public class Main {
 	public static boolean[] visited = new boolean[100001];
 	
 	public static void bfs() {
-		ArrayDeque<Node> deque = new ArrayDeque<>();
+		Deque<Node> deque = new ArrayDeque<>();
 		deque.offer(new Node(n, 0));
 		visited[n] = true;
 		
 		while(!deque.isEmpty()) {
-			Node now = deque.poll();
+			Node now = deque.pollFirst(); // poll();
 			int x = now.x;
 			int t = now.t;
 			
-			// 동생을 찾았다면 시간 출력 후 종료
 			if(x == k) {
 				System.out.println(t);
-				System.exit(0);
+				return;
 			}
 			
-			// 0초 후 - 순간이동을 하는 경우
-			if(2 * x <= 100000 && !visited[2 * x]) {
-				deque.offerFirst(new Node(2 * x, t));
-				visited[2 * x] = true;
+			// 순간 이동: 0초 후 2*X로 이동
+			if(2*x <= 100000 && !visited[2*x]) {
+				deque.offerFirst(new Node(2*x, t));
+				visited[2*x] = true;
 			}
 			
-			// 1초 후 - 걷는 경우
-			if(x - 1 >= 0 && !visited[x - 1]) {
-				deque.offerLast(new Node(x - 1, t + 1));
-				visited[x - 1] = true;
+			// 걷는 경우: 1초 후 X-1 또는 X+1로 이동
+			if(x-1 >= 0 && !visited[x-1]) {
+				deque.offerLast(new Node(x-1, t+1));
+				visited[x-1] = true;
 			}
 			
-			if(x + 1 <= 100000 && !visited[x + 1]) {
-				deque.offerLast(new Node(x + 1, t + 1));
-				visited[x + 1] = true;
+			if(x+1 <= 100000 && !visited[x+1]) {
+				deque.offerLast(new Node(x+1, t+1));
+				visited[x+1] = true;
 			}
+
 		}
+		
 	}
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		
 		bfs();
 	}
+
 }
