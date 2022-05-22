b41de04619b9ae3a1552c83add2b042ec38a60ad
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-05-22 18:18:48 +0900

    update: [13913] 숨바꼭질 4 solution
    백준 13913번 복습 및 풀이 소스코드 수정

--- graph_traversal/[13913] 숨바꼭질 4/풀이 2/Main.java
+++ graph_traversal/[13913] 숨바꼭질 4/풀이 2/Main.java
@@ -1,76 +1,78 @@
+import java.io.*;
 import java.util.*;
 
 class Node {
-	int x;
-	int s; // 시간
+	int x; // 수빈이의 위치
+	int t; // 걸린 시간
 	
-	Node(int x, int s) {
+	Node(int x, int t) {
 		this.x = x;
-		this.s = s;
+		this.t = t;
 	}
 }
 
 public class Main {
-	public static int n, k, ans;
+	public static int n, k;
 	public static boolean[] visited = new boolean[100001];
-	public static int[] prev = new int[100001]; // 이전 위치 저장 배열
+	public static int[] prev = new int[100001]; // 이전 위치
 	public static StringBuilder sb = new StringBuilder();
 	
-	public static void bfs() {
+	public static void bfs(int x) {
 		Queue<Node> q = new LinkedList<>();
-		q.offer(new Node(n, 0));
-		visited[n] = true;
+		q.offer(new Node(x, 0));
+		visited[x] = true;
 		
 		while(!q.isEmpty()) {
 			Node now = q.poll();
-			int x = now.x;
-			int s = now.s;
-			
-			if(x == k) { // 동생을 찾은 경우
-				ans = s;
+			x = now.x;
+			int t = now.t;
+
+			// 동생을 찾은 경우
+			if(x == k) {
+				sb.append(t).append("\n");
 				return;
 			}
 			
-			// x-1로 이동
+			// 걷는 경우 (x-1로 이동)
 			if(x - 1 >= 0 && !visited[x - 1]) {
-				q.offer(new Node(x - 1, s + 1));
+				q.offer(new Node(x - 1, t + 1));
 				visited[x - 1] = true;
 				prev[x - 1] = x; // 이전 위치 기록
 			}
 			
-			// x+1로 이동
+			// 걷는 경우 (x+1로 이동)
 			if(x + 1 <= 100000 && !visited[x + 1]) {
-				q.offer(new Node(x + 1, s + 1));
+				q.offer(new Node(x + 1, t + 1));
 				visited[x + 1] = true;
 				prev[x + 1] = x;
 			}
 			
-			// 2*x로 이동
-			if(2 * x <= 100000 && !visited[2 * x]) {
-				q.offer(new Node(2 * x, s + 1));
-				visited[2 * x] = true;
-				prev[2 * x] = x;
+			// 순간이동 (x*2 로 이동)
+			if(x * 2 <= 100000 && !visited[x * 2]) {
+				q.offer(new Node(x * 2, t + 1));
+				visited[x * 2] = true;
+				prev[x * 2] = x;
 			}
 		}
 	}
 	
-	public static void findPath(int cur) {		
-		// 현재 위치 전에 이동했던 위치가 존재하는 경우
-		if(prev[cur] != -1) findPath(prev[cur]);
+	public static void findPath(int start, int end) {
+		if(start != end) findPath(start, prev[end]);
 		
-		sb.append(cur).append(" ");
+		sb.append(end).append(" ");
 	}
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		k = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken()); // 수빈 위치
+		k = Integer.parseInt(st.nextToken()); // 동생 위치
 		Arrays.fill(prev, -1);
 		
-		bfs();
-		findPath(k); // 이동 경로 구하기
+		bfs(n); // 수빈이가 동생을 찾는 가장 빠른 시간 구하기
+		findPath(n, k); // 이동 경로 구하기
 		
-		System.out.println(ans);
 		System.out.println(sb);
 	}
+	
 }
