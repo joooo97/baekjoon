7f78de36abacc2a72812a8e9267e53912f13c4db
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-09-29 11:48:07 +0900

    update: [16948] 데스 나이트 solution
    백준 16948번 복습 및 풀이 소스코드 수정

--- graph_traversal/[16948] 데스 나이트/Main.java
+++ graph_traversal/[16948] 데스 나이트/Main.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Node {
 	int x;
@@ -12,20 +12,22 @@ class Node {
 }
 
 public class Main {
+	
 	public static int n, r1, c1, r2, c2;
 	public static int[][] d;
-
+	
 	public static int[] dx = {-2, -2, 0, 0, 2, 2};
 	public static int[] dy = {-1, 1, -2, 2, -1, 1};
 	
 	public static void bfs() {
 		Queue<Node> q = new LinkedList<>();
 		q.offer(new Node(r1, c1));
+		d[r1][c1] = 0;
 		
 		while(!q.isEmpty()) {
-			Node now = q.poll();
-			int x = now.x;
-			int y = now.y;
+			Node cur = q.poll();
+			int x = cur.x;
+			int y = cur.y;
 			
 			for(int i = 0; i < 6; i++) {
 				int nx = x + dx[i];
@@ -33,7 +35,7 @@ public class Main {
 				
 				if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
 				
-				if(d[nx][ny] == 0) {
+				if(d[nx][ny] == -1) {
 					q.offer(new Node(nx, ny));
 					d[nx][ny] = d[x][y] + 1;
 				}
@@ -41,18 +43,24 @@ public class Main {
 		}
 	}
 	
-	public static void main(String args[]) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		d = new int[n][n];
+
+		for(int i = 0; i < n; i++) {
+			Arrays.fill(d[i], -1);
+		}
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());	
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		r1 = Integer.parseInt(st.nextToken());
 		c1 = Integer.parseInt(st.nextToken());
 		r2 = Integer.parseInt(st.nextToken());
 		c2 = Integer.parseInt(st.nextToken());
 		
 		bfs();
-		System.out.println(d[r2][c2] == 0 ? -1 : d[r2][c2]);
+		
+		System.out.println(d[r2][c2]);
 	}
+
 }
